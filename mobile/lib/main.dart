c5c454b6a0c170a79bc3d2da24cdb08dfa13c52b
calvinzend <calvinzend@gmail.com>
2026-03-12 21:59:56 +0700

    tes

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mobile HBC',
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
       );
   } 
 }
